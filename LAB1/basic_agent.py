d66fd83e4eee8afa78a060d9673aa8e1a4557af1
DylanGates <mkafoh@st.ug.edu.gh>
2026-01-29 21:52:17 +0000

    fixed spade issue and added a spindown for the basic agent

--- LAB1/basic_agent.py
+++ LAB1/basic_agent.py
@@ -1,18 +1,28 @@
+import asyncio
 import logging
 import spade
 
 logging.basicConfig(level=logging.INFO)
 
+
 class BasicAgent(spade.agent.Agent):
     async def setup(self):
         logging.info("Basic Agent started")
+
 
 async def main():
     agent = BasicAgent("agent1@localhost", "password", verify_security=False)
     await agent.start(auto_register=True)
     logging.info("Agent running")
-    logging.info(" Agent is up and running")
-    await spade.wait_until_finished(agent)
+    logging.info("Agent is up and running")
+    
+    try:
+        await asyncio.sleep(10)  
+    finally:
+        logging.info("Stopping agent...")
+        await agent.stop()
+        logging.info("Agent stopped")
+
 
 if __name__ == "__main__":
     spade.run(main())
